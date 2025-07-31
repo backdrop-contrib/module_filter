5c8276b988dc1e43b9ba2ca3ad351a3e511ea790
izmeez <6208775+izmeez@users.noreply.github.com>
2025-07-31 15:21:45 +0000

    Fixed vertical tabs scroll for jQuery 3.

--- js/module_filter_tab.js
+++ js/module_filter_tab.js
@@ -327,23 +327,25 @@ Backdrop.behaviors.moduleFilterTabs = {
         }
 
         var lastTop = 0;
-        $(window).scroll(function() {
-          var top = viewportTop();
-          var bottom = viewportBottom();
+        $(window).on('scroll', function () {
+          var top = $(window).scrollTop();
+          var bottom = top + $(window).height();
 
-          if ($modules.offset().top >= top) {
-            $tabs.removeClass('top-fixed').attr('style', '');
+          var modulesOffset = $modules.offset().top;
+
+          if (modulesOffset >= top) {
+            $tabs.removeClass('top-fixed bottom-fixed').attr('style', '');
           }
           else {
-            if (top > lastTop) { // Downward scroll.
-              if ($tabs.height() > bottom - top) {
+            if (top > lastTop) { // Scrolling down
+              if ($tabs.outerHeight() > (bottom - top)) {
                 fixToBottom(bottom);
               }
               else {
                 fixToTop(top);
               }
             }
-            else { // Upward scroll.
+            else { // Scrolling up
               fixToTop(top);
             }
           }
