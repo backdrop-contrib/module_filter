19f6437cf4585b192c12c7dd3a42d3295c3c1819
izmeez <6208775+izmeez@users.noreply.github.com>
2026-04-04 10:55:26 -0700

    Fixed vertical tabs scroll jquery3
    * Fixed vertical tabs scroll for jQuery 3.
    
    * Removed functions viewportTop and viewportBottom as no longer used.

--- js/module_filter_tab.js
+++ js/module_filter_tab.js
@@ -272,20 +272,6 @@ Backdrop.behaviors.moduleFilterTabs = {
           return tabsTopOffset;
         }
 
-        function viewportTop() {
-          var top = $(window).scrollTop();
-          return top;
-        }
-
-        function viewportBottom() {
-          var top = $(window).scrollTop();
-          var bottom = top + $(window).height();
-
-          bottom -= $('#page-actions').height();
-
-          return bottom;
-        }
-
         function fixToTop(top) {
           if ($tabs.hasClass('bottom-fixed')) {
             $tabs.css({
@@ -327,23 +313,25 @@ Backdrop.behaviors.moduleFilterTabs = {
         }
 
         var lastTop = 0;
-        $(window).scroll(function() {
-          var top = viewportTop();
-          var bottom = viewportBottom();
+        $(window).on('scroll', function () {
+          var top = $(window).scrollTop();
+          var bottom = top + $(window).height();
+          var modulesOffset = $modules.offset().top;
 
-          if ($modules.offset().top >= top) {
-            $tabs.removeClass('top-fixed').attr('style', '');
+          if (modulesOffset >= top) {
+            $tabs.removeClass('top-fixed bottom-fixed').attr('style', '');
           }
           else {
-            if (top > lastTop) { // Downward scroll.
-              if ($tabs.height() > bottom - top) {
-                fixToBottom(bottom);
-              }
-              else {
-                fixToTop(top);
-              }
+            var scrollingUp = top < lastTop;
+
+            if (scrollingUp) {
+              $tabs.removeClass('top-fixed bottom-fixed').attr('style', '');
             }
-            else { // Upward scroll.
+
+            if ($tabs.outerHeight() > (bottom - top)) {
+              fixToBottom(bottom);
+            }
+            else {
               fixToTop(top);
             }
           }
